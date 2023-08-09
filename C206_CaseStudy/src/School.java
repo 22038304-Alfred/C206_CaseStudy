7afa2c4b6967c85a3e9290aa3c783a4225766a49
22020981 <22020981@DESKTOP-3G0G7FD>
2023-08-09 10:27:27 +0800

    school class

--- C206_CaseStudy/src/School.java
+++ C206_CaseStudy/src/School.java
@@ -27,48 +27,5 @@ public class School {
     public void setAddress(String address) {
         this.address = address;
     }
-    
-    private ArrayList<School> schoolList = new ArrayList<School>();
    
-    
-
-   
-    //add a new school
-    public void addSchool(String name, String address) {
-        schoolList.add(new School(name, address));
-        System.out.println("School has been added");
-    }
-
-    //view all schools
-    public void viewAllSchools() {
-        String sentence = String.format("%-15s %-20s\n", "School Name", "Address");
-       
-
-        for (int i = 0; i < schoolList.size(); i++) {
-            if (schoolList.get(i) != null) {
-                String schoolName = schoolList.get(i).getName();
-                String schoolAddress = schoolList.get(i).getAddress();
-                
-                sentence += String.format("%-15s %-20s\n", schoolName, schoolAddress);
-            }
-        }
-        System.out.println(sentence);
-    }
-
-    //delete an existing school
-    public void deleteSchool(String name) {
-        boolean schoolfound = false;
-        for (int i = 0; i < schoolList.size(); i++) {
-            if (schoolList.get(i) != null && schoolList.get(i).getName().equalsIgnoreCase(name)) {
-                schoolList.remove(i);
-                schoolfound = true;
-                System.out.println("School has been deleted");
-            }
-        }
-        if (!schoolfound) {
-            System.out.println("School not found");
-        }
-    }
-
-
 }
