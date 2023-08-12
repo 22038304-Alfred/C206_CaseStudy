1ee55bf818ba32234952cb9cde7153c3fd3f53a0
22044804 <22044804@LAPTOP-U2MQ31TF>
2023-08-12 12:15:19 +0800

    payment + refactor done by queenie

--- C206_CaseStudy/test/PaymentTest.java
+++ C206_CaseStudy/test/PaymentTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
-import static org.junit.Assert.*;
+import static org.junit.Assert.*; 
 
 
 import java.util.ArrayList;
