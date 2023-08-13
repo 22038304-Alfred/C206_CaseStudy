8e2755c07046443c5fcf316455a71a3107267a18
22020981-Gemma <134348679+22020981-Gemma@users.noreply.github.com>
2023-08-13 15:38:01 +0800

    Update School.java
    Signed-off-by: 22020981-Gemma <134348679+22020981-Gemma@users.noreply.github.com>

--- C206_CaseStudy/src/School.java
+++ C206_CaseStudy/src/School.java
@@ -4,11 +4,13 @@ public class School {
 	
 	private String name;
     private String address;
+    private String postalCode;
     
 
-    public School(String name, String address) {
+    public School(String name, String address, String postalCode) {
         this.name = name;
         this.address = address;
+        this.postalCode = postalCode;
         
     }
 
@@ -27,5 +29,18 @@ public class School {
     public void setAddress(String address) {
         this.address = address;
     }
-   
+    
+    public String getpostalCode() {
+    	return postalCode;
+    }
+    
+    public void setpostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+    
+    public String toString() {
+		String output = String.format("%-30s %-30s %-20\n", name, address, postalCode);
+		return output;
+    }
+
 }
